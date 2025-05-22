9420c22be8f78304323c65ec76870105f40e0e56
anh phu <phudz25022005@gmail.com>
2025-05-22 23:48:35 +0700

    create page login

--- pickleball/frontend/src/routes/AppRoutes.jsx
+++ pickleball/frontend/src/routes/AppRoutes.jsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import LayoutMain from "../layouts/layoutMain";
 import Home from "../modules/Home";
+import LoginPage from "../modules/auth/LoginPage";
 function AppRoutesUser() {
     return (
         <Routes>
             <Route path="/" element={<LayoutMain />}>
                 <Route index element={< Home/>} />
-                <Route path="about" element={<h1>About</h1>} />
                 <Route path="contact" element={<h1>Contact</h1>} />
                 <Route path="*" element={<h1>404 Not Found</h1>} />
             </Route>
+            <Route path="/login" element={<LoginPage />} />
         </Routes>
     );
 }
