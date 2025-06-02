56009040ce2dc89b37077a5abdc441d7e909d623
anh phu <phudz25022005@gmail.com>
2025-06-02 23:45:59 +0700

    add logout

--- pickleball/backend/src/main/java/com/pickle/backend/controller/QuestionController.java
+++ pickleball/backend/src/main/java/com/pickle/backend/controller/QuestionController.java
@@ -6,6 +6,8 @@ import com.pickle.backend.entity.test.Question;
 import com.pickle.backend.repository.OptionRepository;
 import com.pickle.backend.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,20 +23,21 @@ public class QuestionController {
     @Autowired
     private OptionRepository optionRepo;
 
-//    @GetMapping("/quiz")
-//    public List<Question> getQuiz() {
-//        return questionRepo.findRandomQuestions();
-//    }
+    @GetMapping("/quiz")
+    public List<Question> getQuiz() {
+        Pageable pageable = PageRequest.of(0, 10); // Ví dụ: Lấy 10 câu hỏi ngẫu nhiên
+        return questionRepo.findRandomQuestions((java.awt.print.Pageable) pageable);
+    }
 
-//    @PostMapping("/submit")
-//    public int submitQuiz(@RequestBody List<AnswerDTO> answers) {
-//        int score = 0;
-//        for (AnswerDTO answer : answers) {
-//            Optional<Option> selected = optionRepo.findById(answer.getOptionId());
-//            if (selected.isPresent() && selected.get().isCorrect()) {
-//                score++;
-//            }
-//        }
-//        return score;
-//    }
+    @PostMapping("/submit")
+    public int submitQuiz(@RequestBody List<AnswerDTO> answers) {
+        int score = 0;
+        for (AnswerDTO answer : answers) {
+            Optional<Option> selected = optionRepo.findById(answer.getOptionId());
+            if (selected.isPresent() && selected.get().isCorrect()) {
+                score++;
+            }
+        }
+        return score;
+    }
 }
--- pickleball/backend/src/main/java/com/pickle/backend/repository/OptionRepository.java
+++ pickleball/backend/src/main/java/com/pickle/backend/repository/OptionRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.pickle.backend.entity.test.Option;
 
+import java.util.List;
+
 @Repository
 public interface OptionRepository extends JpaRepository<Option, Long> {
-}
+    public List<Option> findById(String id);
