6f88bcfaf3177cb0db312dff81351596c6df498c
anh phu <phudz25022005@gmail.com>
2025-06-18 16:15:50 +0700

    update lesson

---
+++ pickleball/backend/src/main/java/com/pickle/backend/dto/CheckProgressResponseDTO.java
@@ -0,0 +1,38 @@
+package com.pickle.backend.dto;
+
+public class CheckProgressResponseDTO {
+    private boolean isExist;
+    private String message;
+    private long IdProgress;
+
+    public void setIdProgress(long idProgress) {
+        IdProgress = idProgress;
+    }
+
+    public long getIdProgress() {
+        return IdProgress;
+    }
+
+    // Constructor
+    public CheckProgressResponseDTO(boolean isExist, String message) {
+        this.isExist = isExist;
+        this.message = message;
+    }
+
+    // Getters và Setters
+    public boolean getIsExist() {
+        return isExist;
+    }
+
+    public void setIsExist(boolean isExist) {
+        this.isExist = isExist;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
