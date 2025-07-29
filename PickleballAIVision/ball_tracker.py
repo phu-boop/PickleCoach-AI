72522aecde55a4567628be43b5eb23ad5c02a8b0
anh phu <phudz25022005@gmail.com>
2025-07-29 14:19:01 +0700

    edit ball recognition

--- PickleballAIVision/ball_tracker.py
+++ PickleballAIVision/ball_tracker.py
@@ -1,74 +1,68 @@
 import cv2
 import numpy as np
+from config import (
+    BALL_LOWER, BALL_UPPER, BALL_MIN_AREA, BALL_MAX_AREA, BALL_CIRCULARITY,
+    COLOR_BALL, TRAIL_LIFESPAN
+)
 
 class BallTracker:
-    def __init__(self, config):
-        self.ball_positions = []  # Mỗi phần tử: {'pos': (x, y), 'age': 0}
-        self.cfg = config
+    def __init__(self):
+        self.ball_positions = []
 
-    def detect_and_draw_ball(self, frame, overlay, w, h):
-        hsv_full = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        ball_mask = cv2.inRange(hsv_full, self.cfg.BALL_LOWER, self.cfg.BALL_UPPER)
-        ball_mask = cv2.GaussianBlur(ball_mask, (5, 5), 0)
-        contours, _ = cv2.findContours(ball_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    def detect_ball(self, frame, frame_idx):
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, BALL_LOWER, BALL_UPPER)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if not contours:
+            return None
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            perimeter = cv2.arcLength(cnt, True)
+            if perimeter == 0:
+                continue
+            circularity = 4 * np.pi * area / (perimeter ** 2)
+            if BALL_MIN_AREA < area < BALL_MAX_AREA and circularity > BALL_CIRCULARITY:
+                (x, y, w, h) = cv2.boundingRect(cnt)
+                center = (int(x + w / 2), int(y + h / 2))
+                return center
+        return None
 
-        ball_center = None
-        for contour in contours:
-            area = cv2.contourArea(contour)
-            if self.cfg.BALL_MIN_AREA < area < self.cfg.BALL_MAX_AREA:
-                perimeter = cv2.arcLength(contour, True)
-                if perimeter > 0:
-                    circularity = 4 * np.pi * area / (perimeter * perimeter)
-                    if circularity > self.cfg.BALL_CIRCULARITY:
-                        (x_ball, y_ball), radius = cv2.minEnclosingCircle(contour)
-                        center = (int(x_ball), int(y_ball))
-                        cv2.circle(overlay, center, int(radius), self.cfg.COLOR_BALL, -1)
+    def draw_ball(self, frame, ball_center):
+        if ball_center:
+            cv2.circle(frame, ball_center, 10, COLOR_BALL, -1)
 
-                        # Bóng đổ (shadow)
-                        shadow_center = (
-                            center[0] + self.cfg.BALL_SHADOW_OFFSET_X,
-                            center[1] + self.cfg.BALL_SHADOW_OFFSET_Y
-                        )
-                        cv2.ellipse(overlay, shadow_center,
-                                    (self.cfg.BALL_SHADOW_MAJOR, self.cfg.BALL_SHADOW_MINOR),
-                                    0, 0, 360, self.cfg.COLOR_SHADOW, 2)
-
-                        # Thêm vào trail với tuổi = 0
-                        self.ball_positions.append({'pos': center, 'age': 0})
-                        ball_center = center
-                        break
-
-        # Kiểm tra và lọc phần tử hợp lệ
-        self.ball_positions = [pt for pt in self.ball_positions if isinstance(pt, dict) and 'age' in pt and 'pos' in pt]
-
-        # Tăng tuổi cho các điểm trail
-        for pt in self.ball_positions:
-            pt['age'] += 1
-
-        # Xoá điểm quá tuổi
+    def update_trail(self, ball_center, frame_idx):
+        if ball_center:
+            # Tránh thêm trùng điểm liên tiếp
+            if not self.ball_positions or self.ball_positions[-1]["pos"] != ball_center:
+                self.ball_positions.append({"pos": ball_center, "age": 0})
+        for point in self.ball_positions:
+            point["age"] += 1
         self.ball_positions = [
-            pt for pt in self.ball_positions if pt['age'] <= self.cfg.TRAIL_LIFESPAN
+            point for point in self.ball_positions if point["age"] < TRAIL_LIFESPAN
         ]
 
-        # Vẽ đường đi mờ dần + nhỏ dần
+    def draw_ball_trail(self, frame):
+        max_age = TRAIL_LIFESPAN
+        base_color = (0, 0, 255)  # Đỏ đậm
+        base_thickness = 8
         for i in range(1, len(self.ball_positions)):
-            start = self.ball_positions[i - 1]['pos']
-            end = self.ball_positions[i]['pos']
-            age = self.ball_positions[i]['age']
+            p1 = self.ball_positions[i - 1]["pos"]
+            p2 = self.ball_positions[i]["pos"]
+            age = self.ball_positions[i]["age"]
+            alpha = max(0, 1 - age / max_age)
+            color = (
+                int(base_color[0] * alpha),
+                int(base_color[1] * alpha),
+                int(base_color[2] * alpha)
+            )
+            thickness = max(2, int(base_thickness * alpha))
+            cv2.line(frame, p1, p2, color, thickness, lineType=cv2.LINE_AA)
 
-            if (0 <= start[0] < w and 0 <= start[1] < h and
-                0 <= end[0] < w and 0 <= end[1] < h):
-
-                fade = 1.0 - (age / self.cfg.TRAIL_LIFESPAN)
-                fade = max(0.0, fade) ** self.cfg.FADE_FACTOR
-
-                color = (
-                    int(self.cfg.COLOR_BALL_TRAIL[0] * fade),
-                    int(self.cfg.COLOR_BALL_TRAIL[1] * fade),
-                    int(self.cfg.COLOR_BALL_TRAIL[2] * fade)
-                )
-                thickness = max(1, int(self.cfg.TRAIL_THICKNESS * fade))
-
-                cv2.line(overlay, start, end, color, thickness, lineType=cv2.LINE_AA)
-
+    def detect_and_draw_ball(self, frame, overlay, w, h, frame_idx):
+        ball_center = self.detect_ball(frame, frame_idx)
+        self.draw_ball(overlay, ball_center)
+        self.update_trail(ball_center, frame_idx)
+        # Vẽ trail lên overlay để không bị blend che mất
+        self.draw_ball_trail(overlay)
         return ball_center
